69a5a27816d03b67a28fa03d7e4cd010f03e9db1
RayanD2005 <rayan.darazi2005@gmail.com>
2024-11-23 12:56:39 +1300

    fixed Bar Chart bugs

--- app/(routes)/dashboard/_componenets/BarChartDashboard.jsx
+++ app/(routes)/dashboard/_componenets/BarChartDashboard.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import {Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
 
 function BarChartDashboard({budgetList}){
+    const updatedBudgetList = budgetList.map(budget => ({
+        ...budget,
+        remainingBudget: budget.amount - budget.totalSpend,
+      }));
 
     return (
         <div className="border rounded-2xl p-5">
@@ -11,13 +15,20 @@ function BarChartDashboard({budgetList}){
             </h2>
 
             <ResponsiveContainer width={'80%'} height={300}>
-                <BarChart data={budgetList} margin={{top:7}}>
+                <BarChart data={updatedBudgetList} margin={{top:7}}>
                     <XAxis dataKey='name' />
                     <YAxis />
-                    <Tooltip />
+                    <Tooltip 
+                    formatter={(value, name, props) => {
+                    if (name === 'Amount') {
+                        return [props.payload.amount, 'Amount']; // Display the 'amount' value instead of 'remainingBudget'
+                    }
+                    return [value, name];
+                    }} 
+                    />
                     <Legend/>
-                    <Bar dataKey='totalSpend' stackId='a' fill="#4845d2"/>
-                    <Bar dataKey='amount' stackId='a' fill="#c3c2ff"/>
+                    <Bar dataKey='totalSpend' stackId='a' fill="#4845d2" name='Total Spend'/>
+                    <Bar dataKey='remainingBudget' stackId='a' fill="#c3c2ff" name='Amount'/>
                 </BarChart>
             </ResponsiveContainer>
         </div>
